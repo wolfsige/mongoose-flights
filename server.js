6cc5e50ec8d645e782b04c2e6fd741397d870ada
Wolf <d.wolftripp@gmail.com>
2022-02-26 17:41:46 -0800

    link database and server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 import "dotenv/config.js"
+import ("./config/database.js")
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
